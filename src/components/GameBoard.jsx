4fbc8d58cbd1d7d6df01ec37e83cb396cd60df80
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-10-12 12:57:21 +0200

    clicking on board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +6,28 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function UpdateGameBoard(row, column) {
+    setGameBoard((prevGameBoard) => {
+      const UpdateBoard = [
+        ...prevGameBoard.map((innerArrey) => [...innerArrey]),
+      ];
+      UpdateBoard[row][column] = "X";
+      return UpdateBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, Pindex) => (
               <li key={Pindex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => UpdateGameBoard(rowIndex, Pindex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
